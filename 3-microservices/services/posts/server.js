56c6915e7907f4a64a517b2a7791fc5695d1920f
Kuzalin <51911228+Kuzalin@users.noreply.github.com>
2021-04-24 16:20:28 +0530

    Updated
    Updated

--- 3-microservices/services/posts/server.js
+++ 3-microservices/services/posts/server.js
@@ -5,7 +5,6 @@ const db = require('./db.json');
 // Log requests
 app.use(function *(next){
   const start = new Date;
-  
   yield next;
   const ms = new Date - start;
   console.log('%s %s - %s', this.method, this.url, ms);
